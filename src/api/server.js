675e118db22dba521b6b1c6cdc2ac427b2f1c2bb
Thorsten Hans <thorsten.hans@gmail.com>
2016-10-16 12:59:51 +0200

    enable CORS for our api

--- src/api/server.js
+++ src/api/server.js
@@ -6,6 +6,9 @@ const server = restify.createServer({
     name: 'Notes API'
 });
 
+//dev time only!
+server.use(restify.CORS());
+
 registerNoteRoutes(server, require('./controllers/noteController'));
 
 server.listen(port, () => {
