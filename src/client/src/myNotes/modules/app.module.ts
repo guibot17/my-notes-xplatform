598dae2d167c1dea8687148940ee70dd3e903619
Thorsten Hans <thorsten.hans@gmail.com>
2016-10-18 23:49:37 +0200

    cordova-prepare (ng2 build output changed, locationStrategy and outer package added

--- src/client/src/myNotes/modules/app.module.ts
+++ src/client/src/myNotes/modules/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { RootComponent } from '../components/root/root';
 
 @NgModule({
@@ -14,7 +14,9 @@ import { RootComponent } from '../components/root/root';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [RootComponent]
 })
 export class AppModule { }
